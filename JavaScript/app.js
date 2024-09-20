90ea2b684261548f06b71410d4a42b8f568d1ecb
Heidy Aldana <heidycasd@gmail.comm>
2024-09-20 10:26:59 -0500

    clase JavaScript

---
+++ JavaScript/app.js
@@ -0,0 +1,46 @@
+console.log("Hola mundo");
+
+// variables 
+
+
+let numero1 =45;
+let numero2 =39;
+let suma, resta, multiplicacion
+
+const pi = 3.141516;
+
+let estado = true;
+
+let numero3 = 125
+
+
+
+
+
+let direccion = "calle 11 # 40 69";
+
+
+console.log(numero1);
+console.log(numero2);
+console.log(pi);
+console.log(estado);
+console.log(numero3);
+console.log(suma);
+console.log(direccion);
+
+suma = numero1 + numero2;
+resta = numero1 - numero2;
+multiplicacion = numero1 * numero2;
+
+// concatenar valores
+
+console.log('EL PRIMER VALOR ES:' + numero1);
+console.log('EL SEGUNDO VALOR ES:' + numero2);
+console.log('LA SUMA DE' + numero1 + 'y' + numero2 + 'ES:' + suma);
+console.log('LA SUMA DE' , numero2 , 'Y' , numero2 , 'ES:' , suma );
+
+// literal templete
+
+console.log(`LA SUMA DE ${numero1} Y ${numero2} ES: ${suma}`);
+console.log(`LA RESTA DE ${numero1} Y ${numero2} ES: ${resta}`);
+console.log(`LA MULTIPLICACION DE ${numero1} Y ${numero2} ES: ${multiplicacion}`);
